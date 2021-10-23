82be1bb0806cde678418259bf4fe43b8a255aa1c
naocya@naver.com <naocya@naver.com>
2021-10-23 12:11:44 +0900

    final

---
+++ main/java/finalClass/Cat.java
@@ -0,0 +1,11 @@
+// 스프링 입문을 위한 자바 객체 지향의 원리와 이해 158 ~ 160p
+
+package finalClass;
+
+
+// 클래스에 final이 붙으면 상속을 허락하지 않겠다는 의미다.
+// Cat 클래스를 다른 클래스에서 상속 받을려고 하면
+// The type 길고양이 cannot subclass the final class Cat
+// (길고양이 타입은 최종 클래스인 고양이의 하위 분류가 될 수 없다.)
+public final class Cat {
+}
