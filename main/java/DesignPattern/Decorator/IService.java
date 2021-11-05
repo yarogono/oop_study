17aa702ca772048d728acc21cc43e98f8a520714
naocya@naver.com <naocya@naver.com>
2021-11-05 22:33:20 +0900

    Decorator Pattern

---
+++ main/java/DesignPattern/Decorator/IService.java
@@ -0,0 +1,5 @@
+package DesignPattern.Decorator;
+
+public interface IService {
+    public abstract String runSomething();
+}
---
+++ main/java/DesignPattern/Decorator/Service.java
@@ -0,0 +1,9 @@
+package DesignPattern.Decorator;
+
+public class Service implements IService {
+
+    @Override
+    public String runSomething() {
+        return "서비스 짱!!";
+    }
+}
